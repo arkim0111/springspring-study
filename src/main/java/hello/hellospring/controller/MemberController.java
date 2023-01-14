4063df1adaa41cef5ee81bae4a6fe95c5320aab2
arkim0111 <aramingk95@gmail.com>
2023-01-15 00:51:42 +0900

    회원 웹 기능 - 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,5 +16,22 @@ public class MemberController {
     // 그 때 생성자를 호출하는데, 스프링 컨테이너가 memberservice(스프링 빈에 있음)랑 membercontroller를 연결을 시켜줌 => "의존성주입"
         public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+
     }
 }
---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+
+    private String name; // createMemberForm의 name에 등록됨
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
